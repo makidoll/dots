ce9dd319fe2cb6c70623624b12c8dd9636948891
Maki <mxmcube@gmail.com>
2021-11-11 03:14:59 -0600

    Added smb config

--- copy-config-here.sh
+++ copy-config-here.sh
@@ -1,8 +1,12 @@
 rm -rf .config
 
+# home
 mkdir -p .config/alacritty/
 cp -r ~/.config/alacritty/alacritty.yml .config/alacritty/
 
 mkdir -p .config/deadbeef/
 cp -r ~/.config/deadbeef/config .config/deadbeef/
 
+# root
+mkdir -p etc/samba/
+cp -r /etc/samba/smb.conf etc/samba/
